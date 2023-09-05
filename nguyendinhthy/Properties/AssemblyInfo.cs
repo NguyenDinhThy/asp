0b3ead936fd0c3468f7dd8b27cfcf19120a4c779
Nguyễn Đình Thy <129297983+NguyenDinhThy@users.noreply.github.com>
2023-09-05 20:25:28 +0700

    setup

--- nguyendinhthy/Properties/AssemblyInfo.cs
+++ nguyendinhthy/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WebApplication1")]
+[assembly: AssemblyTitle("nguyendinhthy")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WebApplication1")]
+[assembly: AssemblyProduct("nguyendinhthy")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a1087da6-7ecd-4e32-9ca3-4f258a8c9c16")]
+[assembly: Guid("7d08dbf5-f3ef-4db8-8071-ce0b829f5638")]
 
 // Version information for an assembly consists of the following four values:
 //
